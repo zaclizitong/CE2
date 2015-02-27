e5016bcdf12b9caa4e0278d15f8e203f1ba3222f
zaclizitong <zaclizitong@gmail.com>
2015-02-27 23:03:47 +0800

    Same code from CE1

---
+++ TextBuddy.java
@@ -0,0 +1,235 @@
+/*
+This program is a Command Line Interface program called TextBuddy
+Name: LI ZITONG
+Matric Number: A0115635J
+Tutorial Group: 10
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class TextBuddy {
+	
+	//all messages for user
+	private static final String WELCOME_MESSAGE = "Welcome to TextBuddy. %1$s is ready for use";
+	private static final String ERROR_FILE = "Error (File exists)";
+	private static final String ERROR_ADD="Exception in add function";
+	private static final String ERROR_DELETE="Exception in delete function";
+	private static final String ERROR_DISPLAY="Exception in display function";
+	private static final String ERROR_CLEAR="Exception in clear function";
+	
+	private static final String MESSAGE_ADD = "added to %1$s :\"%2$s\" ";
+	private static final String MESSAGE_DELETE = "deleted from %1$s: \" %2$s\"";
+	private static final String MESSAGE_CLEAR = "all content deleted from %1$s";
+	private static final String MESSAGE_EMPTY = "File is empty.";
+	private static final String MESSAGE_DISPLAY = "%1$d. %2$s";
+	
+	//messages for possible errors
+	private static final String ERROR_READ_INPUT="Unable to extract user command from input. System Error: ";
+	private static final String ERROR_INPUT="Error getting user command. System Error: ";
+	private static final String ERROR_PROCESS_COMMAND ="Error executing the command: ";
+	
+	//varibles to read inout
+	private static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	private static String[] inputVector;
+	private static final int splitCommandFromInput = 2; 
+	private static final int numberOfArguments=1;
+	
+
+	//Definition of variables for user commands
+	enum OPTION_TYPE{
+		ADD, DELETE, CLEAR, DISPLAY, EXIT, INVALID
+	};
+	
+
+	private static Vector<String> textBuddyVector = new Vector<String>();
+	
+	public static void main(String args[]){
+		String fileName = args[0];
+		File textBuddy = new File(fileName);
+		createFile(textBuddy);
+		printMessage(fileName);
+		executeCommand(fileName, textBuddy);
+		System.exit(0);
+		
+		
+	}
+	
+	//print welcome message
+	private static void printMessage(String fileName){
+		System.out.println(String.format(WELCOME_MESSAGE, fileName));
+	}
+	
+	//function to create the .txt file based on the name user has chosen
+	private static void createFile(File textbuddy){
+		try{
+			if(!textbuddy.exists()){
+				textbuddy.createNewFile();
+			} 
+		} catch (Exception e){
+			System.err.println(ERROR_FILE + e.getMessage());
+		}
+	}
+	
+	
+/*****************************************************************************************************/	
+	//this section contains methods to read, process and execute user commands
+	private static void executeCommand(String fileName, File textBuddy){
+		
+		OPTION_TYPE userCommand=null;
+
+		do{
+			userCommand=readCommand();
+			processCommand(userCommand, textBuddy);
+		}
+		while(userCommand!= userCommand.EXIT);
+		
+	}
+		
+	//to get the user input and extract the command
+	private static OPTION_TYPE readCommand(){
+		
+		String input;
+		OPTION_TYPE choice=null;
+		
+		try{				
+			System.out.print("command: ");
+			input=in.readLine();	
+			input=input;
+			inputVector=input.split(" ", splitCommandFromInput); 
+			String userInputCommand=inputVector[0];
+				try
+	      {
+	        choice = OPTION_TYPE.valueOf(userInputCommand.toUpperCase());
+	      }
+	      catch(IllegalArgumentException ex)
+	      {
+	      	System.out.println(ERROR_READ_INPUT+ex.getMessage());
+	      	return choice.INVALID;
+	      }
+			return choice;
+		}
+		
+		catch(IOException e){
+			System.out.println(ERROR_INPUT + e.getMessage());
+    	return choice.INVALID;
+		}
+		
+	}
+		
+	//to process the command and call the methods respectively
+	private static void processCommand(OPTION_TYPE command, File textBuddy){
+		
+ 		try{	
+ 			
+	 		switch(command){
+		
+	 		case EXIT:
+					break;
+				
+	 		case ADD:
+					addItem(inputVector[1], textBuddy);
+					break;	
+				
+	 		case DELETE:
+					int index= Integer.parseInt(inputVector[1]); 
+					deleteItem(index, textBuddy);
+					break;
+				
+	 		case CLEAR:
+					clearItem(textBuddy);
+					break;
+				
+	 		case DISPLAY:
+					displayFile();
+					break;
+					
+	 		}
+ 		}
+	
+ 		catch(Exception e){
+ 			System.out.println(ERROR_PROCESS_COMMAND + e.getMessage());
+ 		}
+	}
+	
+/*****************************************************************************************************/
+	
+	//read data from the .txt file
+	private static void readFromFile(File textBuddy) throws Exception {	
+		
+		FileInputStream fstream = new FileInputStream(textBuddy);
+		DataInputStream in = new DataInputStream(fstream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String strLine;
+		String readFromFile; 
+
+		while ((strLine = br.readLine()) != null) { 
+			textBuddyVector.add(strLine);
+		}
+		in.close();
+	}
+	//write data back to .txt file
+	private static void writeToFile(File textBuddy) throws Exception {
+		textBuddy.delete();
+		textBuddy.createNewFile();
+		String newLine="\n";
+		BufferedWriter outFile = new BufferedWriter(new FileWriter(textBuddy));
+
+		for (int i = 0; i < textBuddyVector.size(); i++) {
+			outFile.write(textBuddyVector.elementAt(i) + newLine);   
+		}
+
+		outFile.close();
+	}
+	
+	//add item to the vector and write to the .txt file
+	private static void addItem(String userInput, File textBuddy){
+		try {
+			textBuddyVector.add(userInput);
+			writeToFile(textBuddy);
+			System.out.println(String.format(MESSAGE_ADD, textBuddy, userInput));
+		} catch (Exception e) {
+			System.out.println(ERROR_ADD + e.getMessage());
+		}
+	}
+	
+	//delete item from the .txt file
+	private static void deleteItem(int deleteIndex, File textBuddy){
+			try {
+				String strToDelete;
+				strToDelete = new String(textBuddyVector.elementAt(deleteIndex - 1).toString());   
+				textBuddyVector.remove(deleteIndex - 1);
+				writeToFile(textBuddy);
+				System.out.println(String.format(MESSAGE_DELETE, textBuddy,
+						strToDelete));
+			} catch (Exception e) {
+				System.out.println(ERROR_DELETE + e.getMessage());
+			}
+	}
+	
+	//display elements in the .txt file
+	private static void displayFile(){
+		try {
+			if(textBuddyVector.size()==0)
+				System.out.println(MESSAGE_EMPTY);
+			for (int i = 0; i < textBuddyVector.size(); i++) {
+				System.out.println(String.format(MESSAGE_DISPLAY, (i + 1),
+						textBuddyVector.elementAt(i)));
+			}
+		} catch (Exception e) {
+			System.out.println(ERROR_DISPLAY + e.getMessage());
+		}
+	}
+	
+	//clear everything in the .txt file
+	private static void clearItem(File textBuddy){
+		try {
+			textBuddyVector.removeAllElements();
+			System.out.println(String.format(MESSAGE_CLEAR, textBuddy));
+			writeToFile(textBuddy);
+		} catch (Exception e) {
+			System.out.println(ERROR_CLEAR + e.getMessage());
+		}
+	}
+		
+}
